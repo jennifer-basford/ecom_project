6533d8be2f5249fc58f5eae9f2c133052800213a
jenniferbasford <basford.jennifer@gmail.com>
2020-09-11 11:31:34 -0600

    finished sign up form and grid

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -11,7 +11,7 @@ class SignUp extends Component {
   render() {
     return (
       <div className="sign-up">
-        <PageTitle className="sign-up__page-title" title="Login" />
+        <PageTitle className="sign-up__page-title" title="Register" />
         <SignUpForm onSubmit={this.onSubmit} className="sign-up__form" />
       </div>
     );
