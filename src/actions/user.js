7e71ac28e31d26cc931672feb024de1385d0c0a7
jenniferbasford <basford.jennifer@gmail.com>
2020-09-16 13:21:24 -0600

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -5,14 +5,92 @@ export function fetchUserPurchases() {
     type: SET_USER_PURCHASES,
     payload: [
       {
-        id: 0,
-        title: "purchase 1",
-        amount: 8.02,
+        _id: 0,
+        total: 19.4,
+        orderNumber: "A0048248343",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Jordan Hudgens",
+          shippingAddress: "1234 West State Street",
+        },
       },
       {
-        id: 1,
-        title: "purchase 2",
-        amount: 19.4,
+        _id: 1,
+        total: 1.3,
+        orderNumber: "24343sdfkjh72",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Maxwell Nelson",
+          shippingAddress: "1337 West State Street",
+        },
+      },
+      {
+        _id: 2,
+        total: 1.3,
+        orderNumber: "B0048248343",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Jordan Hudgens",
+          shippingAddress: "1234 West State Street",
+        },
+      },
+      {
+        _id: 3,
+        total: 16.4,
+        orderNumber: "ADFasdlfkjwe",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Max Asher Nelson",
+          shippingAddress: "1234 West State Street",
+        },
+      },
+      {
+        _id: 4,
+        total: 19.4,
+        orderNumber: "A0048248343",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Jordan Hudgens",
+          shippingAddress: "1234 West State Street",
+        },
+      },
+      {
+        _id: 5,
+        total: 19.4,
+        orderNumber: "A0048248343",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Jordan Hudgens",
+          shippingAddress: "1234 West State Street",
+        },
+      },
+      {
+        _id: 6,
+        total: 19.4,
+        orderNumber: "A0048248343",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Jordan Hudgens",
+          shippingAddress: "1234 West State Street",
+        },
+      },
+      {
+        _id: 7,
+        total: 3.2,
+        orderNumber: "5UFISHDFJASUSHI298",
+        orderDate: new Date(),
+        creditCard: "-0000",
+        user: {
+          name: "Andrew Wold",
+          shippingAddress: "348 Yo State Street",
+        },
       },
     ],
   };
