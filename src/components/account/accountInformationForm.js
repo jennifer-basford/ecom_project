f7c08438f7e53b393f8b95210b958a985f8f28b0
jenniferbasford <basford.jennifer@gmail.com>
2020-09-18 09:24:25 -0600

    built long gray button component and placed instance of it on account information form to create change password button

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { reduxForm, Field } from "redux-form";
 
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton, LongGrayButton } from "../formFields";
 
 import history from "../../history";
 
@@ -72,16 +72,17 @@ class AccountInformationForm extends Component {
           placeholder="Password"
           name="password"
           component={FormInput}
-        />
-        
-        <Field
-          className="account-information-form__login"
-          onClick={() => history.push("/account")}
-          type="submit"
-          title="Login"
-          name="login"
-          component={FormButton}
         /> */}
+
+        <Field
+          className="account-information-form__change-password"
+          onClick={() => console.log("trying to show buttons")}
+          type="button"
+          labelTitle="Password"
+          title="Change Password"
+          name="change-password"
+          component={LongGrayButton}
+        />
       </form>
     );
   }
