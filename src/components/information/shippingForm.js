600ba8ad9ad102ccf5233b6e7408fb111745d22f
jenniferbasford <basford.jennifer@gmail.com>
2020-09-28 16:37:39 -0600

    finished shipping form component and sub components

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -74,7 +74,7 @@ class ShippingForm extends Component {
           component={FormButton}
         />
 
-        <OrderSummary className="shipping-form__summary" />
+        <OrderSummary className="shipping-form__order-summary" />
       </form>
     );
   }
